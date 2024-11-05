ae4231afb209943635bd5c93622c7e7815962de0
PodgeMc <padraig.a.d.mccormack@outlook.com>
2024-11-05 12:58:09 +0000

    Added intro text

--- Text_Based_Game/Text_Based_Game/Text_Based_Game.cpp
+++ Text_Based_Game/Text_Based_Game/Text_Based_Game.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    std::cout << R"(                                                                                                                                
+88888888ba               88                                          88888888ba                                       88         
+88      "8b              ""                                          88      "8b                                      88         
+88      ,8P                                                          88      ,8P                                      88         
+88aaaaaa8P'  8b,dPPYba,  88  ,adPPYba,   ,adPPYba,   8b,dPPYba,      88aaaaaa8P'  8b,dPPYba,   ,adPPYba,  ,adPPYYba,  88   ,d8   
+88""""""'    88P'   "Y8  88  I8[    ""  a8"     "8a  88P'   `"8a     88""""""8b,  88P'   "Y8  a8P_____88  ""     `Y8  88 ,a8"    
+88           88          88   `"Y8ba,   8b       d8  88       88     88      `8b  88          8PP"""""""  ,adPPPPP88  8888[      
+88           88          88  aa    ]8I  "8a,   ,a8"  88       88     88      a8P  88          "8b,   ,aa  88,    ,88  88`"Yba,   
+88           88          88  `"YbbdP"'   `"YbbdP"'   88       88     88888888P"   88           `"Ybbd8"'  `"8bbdP"Y8  88   `Y8a                                                                                                             
+)" << std::endl;
+    return 0;
 }
