0ee8b11d09468172ea315f18cb3c4235e9dacca4
StudentMik <A00313685@student.tus.ie>
2024-11-27 19:38:36 +0000

    updated enemy and chance script

--- Text_Based_Game_Project/ChanceManager.h
+++ Text_Based_Game_Project/ChanceManager.h
@@ -2,30 +2,36 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include "Player.h"
+
+using namespace std;
 
 class chances {
+public:
 
+	Player player;
 
 	void fight()
 	{
 		std::srand(std::time(0));
 
 		int opponetDamage = rand() % 101;
-		//bool FightPicked;
 
 
 
 
-		int newHP = opponetDamage -= ;
+
+		int newHP = opponetDamage -= player.getHealth();
+		int gainedScore = player.getScore() + 2;
 		std::cout << "player hp: " << newHP << "\n";
-		//	cout << "XP: " << gainedXP << '\n';
 		if (newHP < 50)
 		{
 			cout << "was it really worth it?\n";
 		}
 
+	}
 
 
 
 
-	};
+};
--- Text_Based_Game_Project/Player.h
+++ Text_Based_Game_Project/Player.h
@@ -2,10 +2,39 @@
 #include <string>
 class Player {
 private:
-	std::string name;
-	int health;
+	std::string name; // Player's name
+	int health;  // Player's health
+	int score; // Player's score
 
 
 public:
-	//Player(std::string playerName);
 
+	// Constructor to initialize player
+	Player(std::string playerName, int initialHealth = 100, int initialScore = 0) : name(playerName), health(initialHealth), score(initialScore) {};
+
+
+	int getHealth()
+	{
+		return health;
+	}
+
+	int getScore()
+	{
+		return score;
+	}
+
+	string getName()
+	{
+		return name;
+	};
+
+	// Method to take damage
+	//void takeDamage(int damage);
+
+	// Method to heal
+	//void heal(int amount);
+
+	// Method to display player's stats
+//	void displayStats();
+
+};
