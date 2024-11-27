dc393d439270284b9ec8d7001d7179b8d5a7a6c3
PodgeMc <padraig.a.d.mccormack@outlook.com>
2024-11-27 19:19:00 +0000

    player script started

--- Text_Based_Game_Project/Player.h
+++ Text_Based_Game_Project/Player.h
@@ -1,11 +1,22 @@
 #pragma once
 #include <string>
+
 class Player {
-private:
-	std::string name;
-	int health;
-
-
 public:
-	//Player(std::string playerName);
+    // Member variables
+    std::string name;  // Player's name
+    int health;        // Player's health
+    int score;         // Player's score
 
+    // Constructor to initialize player
+    Player(std::string playerName, int initialHealth = 100, int initialScore = 0);
+
+    // Method to take damage
+    void takeDamage(int damage);
+
+    // Method to heal
+    void heal(int amount);
+
+    // Method to display player's stats
+    void displayStats();
+};
