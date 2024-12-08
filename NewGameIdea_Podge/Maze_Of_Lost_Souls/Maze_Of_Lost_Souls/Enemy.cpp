7017fe4cd5234237538230b0f030807d4ba01c3c
StudentMik <157600132+StudentMik@users.noreply.github.com>
2024-12-08 18:54:19 +0000

    test 2

---
+++ NewGameIdea_Podge/Maze_Of_Lost_Souls/Maze_Of_Lost_Souls/Enemy.cpp
@@ -0,0 +1,5 @@
+#include "Enemy.h"
+#include <iostream>
+
+//constructor 
+Enemy::Enemy(int health, int attack) : _health(health), _attackDamage(attack) {}
--- NewGameIdea_Podge/Maze_Of_Lost_Souls/Maze_Of_Lost_Souls/Enemy.h
+++ NewGameIdea_Podge/Maze_Of_Lost_Souls/Maze_Of_Lost_Souls/Enemy.h
@@ -1,1 +1,24 @@
+
 #pragma once
+#include <vector>
+#include "Point2D.h"
+#include "Map.h"
+
+class Enemy
+{
+private:
+
+    int _health;
+    int _attackDamage;
+
+
+public:
+    Enemy(int health, int attack);
+
+
+    // int getHealth() const;
+
+
+
+
+};
